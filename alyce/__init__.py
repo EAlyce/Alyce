d2bcddc715f56f908604dc1234e38d0b5deff676
EAlyce <msosjaahwwj@gmail.com>
2025-07-31 22:28:06 +0800

    chore: 自动递增 Alyce 版本号 [ci skip]

--- alyce/__init__.py
+++ alyce/__init__.py
@@ -2,6 +2,7 @@
 Alyce - Telegram client main package
 """
 
+# Alyce 版本号自动维护，最大 99.99.99
 __version__ = "1.0.0"
 __author__ = "EAlyce"
 __description__ = "Alyce: Modern Telegram client and plugin framework"
