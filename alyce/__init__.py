610ca60756281b26659ce8bb75cd7398a1455510
EAlyce <msosjaahwwj@gmail.com>
2025-07-31 20:26:41 +0800

    1

--- alyce/__init__.py
+++ alyce/__init__.py
@@ -7,13 +7,11 @@ __author__ = "EAlyce"
 __description__ = "Alyce: Modern Telegram client and plugin framework"
 
 from core.client.telegram import TelegramClient
-from core.plugin.manager import PluginManager
 from utils.config import config
 from utils.logging import setup_logger
 
 __all__ = [
     'TelegramClient',
-    'PluginManager',
-    'config',
+        'config',
     'setup_logger',
 ]
